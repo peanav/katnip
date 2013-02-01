ae1ef31c9539096c16bc8b4902625a8090298c69
Jason Staten <jstaten07@gmail.com>
2013-02-01 12:05:27 -0700

    404 when coffee/less not found

--- modules/app.rb
+++ modules/app.rb
@@ -1,16 +1,22 @@
 class KatalusApp < Sinatra::Base
-
   get '/' do
     slim :index
   end
 
   get '/styles/*.css' do |sheet_name|
     content_type :css
-    less :"less/#{sheet_name}", :paths => ['views/less', 'vendor/bootstrap/less']
+    begin
+      less :"less/#{sheet_name}", :paths => ['views/less', 'vendor/bootstrap/less']
+    rescue Errno::ENOENT
+      status 404
+    end
   end
 
   get '/scripts/*.js' do |script_name|
-    coffee :"/coffee/#{script_name}", :bare => true
+    begin
+      coffee :"/coffee/#{script_name}", :bare => true
+    rescue Errno::ENOENT
+      status 404
+    end
   end
-
 end
